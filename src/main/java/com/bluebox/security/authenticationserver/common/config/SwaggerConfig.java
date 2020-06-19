46d6e9ffe7a680b4550d5f9db971c6bd4228042b
kamran.ghiasvand <kamran.ghiasvand@peykasa.ir>
2020-06-19 09:46:24 +0430

    add hateos change swagger documentation

--- src/main/java/com/bluebox/security/authenticationserver/common/config/SwaggerConfig.java
+++ src/main/java/com/bluebox/security/authenticationserver/common/config/SwaggerConfig.java
@@ -4,14 +4,21 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Yaser(amin) Sadeghi
@@ -33,15 +40,22 @@ public class SwaggerConfig {
 
     private ApiInfo getApiInfo() {
         return new ApiInfo(
-                "PA OLAP X",
-                "PA OLAP X",
+                "Authentication Server",
+                "This application provides api(s) to create and maintain authentication and authorization",
                 "",
-                "",
-                null,
+                "all right reserved to www.bluebox.ir",
+                new Contact("Kamran Ghiasvand","","kamran.ghiasvand@gmail.com"),
                 "",
                 null,
                 Collections.emptyList()
         );
+    }
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
     }
 
 }
