436b11421e323207c6806b5e8a9809307e222b26
kamran ghiasvand <kamran.ghiasvand@gmail.com>
2020-07-02 22:28:14 +0430

    add LogSmsServiceTest

--- src/test/java/com/bluebox/security/authenticationserver/persistence/service/sms/LogSmsServiceTest.java
+++ src/test/java/com/bluebox/security/authenticationserver/persistence/service/sms/LogSmsServiceTest.java
@@ -1,87 +1,68 @@
 package com.bluebox.security.authenticationserver.persistence.service.sms;
 
 import com.bluebox.security.authenticationserver.common.exception.GlobalException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.MessageFormat;
+
 import static com.bluebox.security.authenticationserver.common.Constants.*;
 import static java.text.MessageFormat.format;
+import static org.mockito.Mockito.*;
 
 /**
  * @author by kamran ghiasvand
  */
-@SpringBootTest(properties = {"app.production-mode=true"})
+@SpringBootTest(properties = {"app.production-mode=false"})
 @ExtendWith({SpringExtension.class})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Transactional
-public class MeliPaymentSmsServiceTest {
+public class LogSmsServiceTest {
     private final String smsContent = "this is a test message from auth-server MeliPaymentSmsServiceTest";
     private final String phone = "+989333938680";
     private final String TEXT_FIELD_NAME = "text";
     private final String PHONE_FIELD_NAME = "phone";
 
+    @Mock
+    private Appender mockAppender;
+    @Captor
+    private ArgumentCaptor<LogEvent> captorLoggingEvent;
+    private Logger logger;
     @Autowired
     private SmsService smsService;
 
+    @BeforeEach
+    public void beforeEach() {
+        when(mockAppender.getName()).thenReturn("MockAppender");
+        when(mockAppender.isStarted()).thenReturn(true);
+        when(mockAppender.isStopped()).thenReturn(false);
+        logger = (Logger) LogManager.getLogger(LogSmsServiceImpl.class);
+        logger.addAppender(mockAppender);
+        logger.setLevel(Level.INFO);
+    }
+
     @Test
     @Order(1)
-    public void successfullySend() throws GlobalException {
+    public void loggingIsWorking() throws GlobalException {
         smsService.sendText(phone, smsContent);
+        verify(mockAppender,times(1)).append(captorLoggingEvent.capture());
+        Assertions.assertEquals(1,captorLoggingEvent.getAllValues().size());
+        Assertions.assertEquals(MessageFormat.format("sending text ''{0}'' to ''{1}''",smsContent,phone),
+                captorLoggingEvent.getAllValues().get(0).getMessage().getFormattedMessage());
     }
 
-    @Test
-    @Order(2)
-    public void nullPhone() {
-        final var exception = Assertions.assertThrows(GlobalException.class, () -> smsService.sendText(null, smsContent));
-        Assertions.assertNotNull(exception.getMessages());
-        Assertions.assertNotEquals(0, exception.getMessages());
-        final var message = exception.getMessages().get(0);
-        Assertions.assertEquals(format(VALIDATION_IS_NULL_MSG, PHONE_FIELD_NAME), message);
-    }
-
-    @Test
-    @Order(3)
-    public void emptyPhone() {
-        final var exception = Assertions.assertThrows(GlobalException.class, () -> smsService.sendText("", smsContent));
-        Assertions.assertNotNull(exception.getMessages());
-        Assertions.assertNotEquals(0, exception.getMessages());
-        final var message = exception.getMessages().get(0);
-        Assertions.assertEquals(format(VALIDATION_REGEX_NOT_VALID_MSG, PHONE_FIELD_NAME), message);
-    }
-
-    @Test
-    @Order(4)
-    public void invalidPhone() {
-        final var exception = Assertions.assertThrows(GlobalException.class, () -> smsService.sendText("invalid", smsContent));
-        Assertions.assertNotNull(exception.getMessages());
-        Assertions.assertNotEquals(0, exception.getMessages());
-        final var message = exception.getMessages().get(0);
-        Assertions.assertEquals(format(VALIDATION_REGEX_NOT_VALID_MSG, PHONE_FIELD_NAME), message);
-    }
-
-    @Test
-    @Order(5)
-    public void nullText() {
-        final var exception = Assertions.assertThrows(GlobalException.class, () -> smsService.sendText(phone, null));
-        Assertions.assertNotNull(exception.getMessages());
-        Assertions.assertNotEquals(0, exception.getMessages());
-        final var message = exception.getMessages().get(0);
-        Assertions.assertEquals(format(VALIDATION_IS_NULL_MSG, TEXT_FIELD_NAME), message);
-    }
-
-    @Test
-    @Order(3)
-    public void emptyText() {
-        final var exception = Assertions.assertThrows(GlobalException.class, () -> smsService.sendText(phone, ""));
-        Assertions.assertNotNull(exception.getMessages());
-        Assertions.assertNotEquals(0, exception.getMessages());
-        final var message = exception.getMessages().get(0);
-        Assertions.assertEquals(format(VALIDATION_IS_EMPTY_MSG, TEXT_FIELD_NAME), message);
-    }
 
 
 }
